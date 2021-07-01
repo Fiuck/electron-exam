519c4b1a0f1fee501afe14e8c6122f3d2b034a34
lemcoo <torrentox@163.com>
2021-07-01 21:08:51 +0800

    first commit

---
+++ src/renderer/utils/request.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import router from "../router";
+import Element from "element-ui";
+
+export function request(config){
+  const instance = axios.create({
+    baseURL: "",
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  })
+
+  // 请求拦截器
+  instance.interceptors.request.use( config => {
+    return config
+  }, error => {})
+
+  // 响应拦截器
+  instance.interceptors.response.use( response => {
+    console.log('response => ' + response);
+
+    let r = response.data
+
+    if(r.code != 200){
+      Element.Message.error(!r.message ? '系统异常' : r.msg)
+      return Promise.reject(r.msg)
+    }
+    return response
+  }, error => {
+    console.log('error => ' + error);
+    
+  })
+
+  return instance(config)
+}
