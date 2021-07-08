0a938a4fecec3f6da8315ef556217c25f1b5419a
zhaowx <zhaoweixu@zg-sst.com>
2021-07-08 10:32:42 +0800

    success build exe

---
+++ src/renderer/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <div class="login-container">
+      <div class="login-wrapper">
+        <div class="login-img">
+          <img src="~@/assets/img/img-01.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
