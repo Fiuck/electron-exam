a7a64d5e770c0c77abce8055fb9bde1cf618b068
zhaowx <zhaoweixu@qq.com>
2021-07-08 22:33:52 +0800

    add login-form

--- src/main/index.js
+++ src/main/index.js
@@ -1,18 +1,22 @@
-import { app, BrowserWindow, Menu } from 'electron'
-import '../renderer/store'
+import { app, BrowserWindow, ipcMain } from "electron"
+import "../renderer/store"
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\")
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const ipc = ipcMain
+const winURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
 
 function createWindow() {
   /**
@@ -20,30 +24,39 @@ function createWindow() {
    */
   mainWindow = new BrowserWindow({
     useContentSize: false,
-    fullscreen: true,  // 全屏
+    fullscreen: true, // 全屏
     frame: false, // 关闭工具栏
-    resizable: false //禁止改变主窗口尺寸
+    resizable: false, //禁止改变主窗口尺寸
+    webPreferences: {
+      nodeIntegration: true, //在网页中集成Node
+    },
   })
 
   mainWindow.loadURL(winURL)
 
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     mainWindow = null
   })
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow)
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit()
   }
 })
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createWindow()
   }
+})
+
+// 窗口事件监听
+// 监听关闭按钮事件
+ipc.on("close", () => {
+  app.exit()
 })
 
 /**
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
+import Vue from "vue"
 
-import App from './App'
-import router from './router'
-import store from './store'
+import App from "./App"
+import router from "./router"
+import store from "./store"
 
 // 引如ant-design-vue
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import Antd from "ant-design-vue"
+import "ant-design-vue/dist/antd.css"
 
 Vue.use(Antd)
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+if (!process.env.IS_WEB) Vue.use(require("vue-electron"))
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
@@ -18,5 +18,5 @@ new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>'
-}).$mount('#app')
+  template: "<App/>",
+}).$mount("#app")
